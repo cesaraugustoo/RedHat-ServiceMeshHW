2279b7b00d7be73fb050ab21059bddf2510df604
cesaraugustoo <33937727+cesaraugustoo@users.noreply.github.com>
2020-03-05 16:31:05 -0300

    Update bookinfo_inject_istio_annotation.sh

--- bookinfo_inject_istio_annotation.sh
+++ bookinfo_inject_istio_annotation.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-BI_Deployments="
-        details-v1 \
-        productpage-v1 \
-        ratings-v1 \
-        reviews-v1 \
-        reviews-v2 \
-        reviews-v3"
+	BI_Deployments="
+			details-v1 \
+			productpage-v1 \
+			ratings-v1 \
+			reviews-v1 \
+			reviews-v2 \
+			reviews-v3"
 
 # Responsible for injecting the istio annotation that opts in a deployment for auto injection of the envoy sidecar
 function injectAndResume() {
@@ -51,6 +51,6 @@ spec:
 for D_NAME in $BI_Deployments;
 do
   POD_NAME=$(oc get pods -n bookinfo -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep $D_NAME)
-  IMG=$(oc get pod $POD_NAME -n bookinfo -o jsonpath='{.spec.containers[*].image}')
+  IMG=$(oc get pod $POD_NAME -n bookinfo -o jsonpath='{.spec.containers[*].image}' | grep $D_NAME)
   injectAndResume
 done
